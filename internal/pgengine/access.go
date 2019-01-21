97ca16d4ca16a9c17d0491f70caafbded73f7b47
Pavlo Golub <pavlo.golub@gmail.com>
2019-01-21 12:45:31 +0200

    pgengine added

---
+++ internal/pgengine/access.go
@@ -0,0 +1,41 @@
+package pgengine
+
+import (
+	"fmt"
+	"os"
+)
+
+//VerboseLogLevel specifies if log messages with level LOG should be logged
+var VerboseLogLevel = true
+
+// Performs logging to configuration database ConfigDB initiated during bootstrap
+func LogToDB(instanceId int, level string, msg ...interface{}) {
+	if level == "LOG" && !VerboseLogLevel {
+		return
+	}
+	fmt.Printf("[%s:%d]:\t%s\n", level, instanceId, fmt.Sprint(msg...))
+	if instanceId == 0 {
+		ConfigDb.MustExec(`INSERT INTO cyberbackup.t_log(pid, database_host_id, log_level, message) 
+				VALUES ($1, NULL, $2, $3)`, os.Getpid(), level, fmt.Sprint(msg...))
+	} else {
+		ConfigDb.MustExec(`INSERT INTO cyberbackup.t_log(pid, database_host_id, log_level, message) 
+			VALUES ($1, $2, $3, $4)`, os.Getpid(), instanceId, level, fmt.Sprint(msg...))
+	}
+	if level == "PANIC" {
+		panic(fmt.Sprint(msg...))
+	}
+}
+
+/*FixSchedulerCrash make sure that task chains which are not complete due to a scheduler crash are "fixed"
+and marked as stopped at a certain point */
+func FixSchedulerCrash() {
+	ConfigDb.MustExec(
+		`INSERT INTO scheduler.run_status (execution_status, started, last_status_update, start_status)
+  SELECT 'SCHEDULER_DEATH', now(), now(), start_status FROM (
+   SELECT   start_status
+     FROM   scheduler.run_status
+     WHERE   execution_status IN ('STARTED', 'CHAIN_FAILED',
+                'CHAIN_DONE', 'SCHEDULER_DEATH')
+     GROUP BY 1
+     HAVING count(*) < 2 ) AS abc`)
+}
---
+++ internal/pgengine/bootstrap.go
@@ -0,0 +1,59 @@
+package pgengine
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq" // postgresql driver blank import
+)
+
+// ConfigDb is the global database object
+var ConfigDb *sqlx.DB
+
+// InitAndTestConfigDBConnection opens connection and creates schema
+func InitAndTestConfigDBConnection(host, port, dbname, user, password, sslmode, schemafile string) {
+	var err error
+
+	ConfigDb, err = sqlx.Connect("postgres", fmt.Sprintf("host=%s port=%s dbname=%s sslmode=%s user=%s password=%s",
+		host, port, dbname, sslmode, user, password))
+	if err != nil {
+		log.Fatalln("Could not open configDb connection! Exit.", err)
+	}
+
+	if err := ConfigDb.Ping(); err != nil {
+		log.Fatalln(err)
+	}
+
+	var exists bool
+	row := ConfigDb.QueryRow("SELECT EXISTS(SELECT 1 FROM pg_namespace WHERE nspname = 'cyberbackup')")
+	if err := row.Scan(&exists); err != nil {
+		log.Fatalln(err)
+	}
+	if !exists {
+		createConfigDBSchema(schemafile)
+		LogToDB(0, "LOG", "Configuration schema created...")
+	}
+	LogToDB(0, "LOG", "Connection established...")
+}
+
+func createConfigDBSchema(schemafile string) {
+	b, err := ioutil.ReadFile(schemafile) // nolint: gosec
+	if err != nil {
+		log.Fatalln("Cannot open schema file.", err)
+	}
+	_, err = ConfigDb.Exec(string(b))
+	if err != nil {
+		log.Fatalln(err)
+	}
+	LogToDB(0, "LOG", fmt.Sprintf("Created cyberbackup schema from file: %s", schemafile))
+}
+
+// FinalizeConfigDBConnection closes session
+func FinalizeConfigDBConnection() {
+	LogToDB(0, "LOG", "Closing session")
+	if err := ConfigDb.Close(); err != nil {
+		log.Fatalln("Cannot close database connection:", err)
+	}
+}
