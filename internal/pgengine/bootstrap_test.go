f652793ac1e0e760fd8673a7f57ba04377e54a29
Pavlo Golub <pavlo.golub@gmail.com>
2019-02-12 15:05:09 +0200

    setup and teardown methods added to bootstrap testing

--- internal/pgengine/bootstrap_test.go
+++ internal/pgengine/bootstrap_test.go
@@ -6,6 +6,24 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+func setupTestCase(t *testing.T) func(t *testing.T) {
+	t.Log("setup test case")
+	return func(t *testing.T) {
+		ConfigDb.MustExec("DROP IF EXISTS SCHEMA timetable")
+		t.Log("test schema dropped")
+	}
+}
+
 func TestBootstrapSQLFileExists(t *testing.T) {
 	require.FileExists(t, "../../sql/"+SQLSchemaFile, "Bootstrap file doesn't exist")
+}
+
+func TestInitAndTestConfigDBConnection(t *testing.T) {
+	teardownTestCase := setupTestCase(t)
+	defer teardownTestCase(t)
+
+	InitAndTestConfigDBConnection("localhost", "5432", "timetable", "scheduler",
+		"scheduler", "disable", "../../sql/"+SQLSchemaFile)
+
+	require.NotNil(t, ConfigDb, "ConfigDB should be initialized")
 }
