0abc08494788746d557822216e19f7e182835f35
Pavlo Golub <pavlo.golub@gmail.com>
2019-03-05 14:36:07 +0200

    leftover from pgengine test removed

--- internal/tasks/tasks_test.go
+++ internal/tasks/tasks_test.go
@@ -3,20 +3,8 @@ package tasks
 import (
 	"testing"
 
-	"github.com/cybertec-postgresql/pg_timetable/internal/pgengine"
 	"github.com/stretchr/testify/assert"
 )
-
-func setupTestCase(t *testing.T) func(t *testing.T) {
-	pgengine.ClientName = "scheduler_unit_test"
-	pgengine.InitAndTestConfigDBConnection("localhost", "5432", "timetable", "scheduler",
-		"scheduler", "disable", pgengine.SQLSchemaFiles)
-	return func(t *testing.T) {
-		pgengine.ConfigDb.MustExec("DROP SCHEMA IF EXISTS timetable CASCADE")
-		t.Log("Test schema dropped")
-		pgengine.ConfigDb.Close()
-	}
-}
 
 func TestDownloadFile(t *testing.T) {
 	downloadUrls = func(urls []string, dest string, workers int) error { return nil }
