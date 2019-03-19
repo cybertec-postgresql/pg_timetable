cdf33d8d7385c816aba91ca7a6675e9c53883423
Pavlo Golub <pavlo.golub@gmail.com>
2019-03-19 14:51:27 +0200

    NoOp sample rewritten using insert_base_task function

--- samples/NoOp.sql
+++ samples/NoOp.sql
@@ -1,18 +1,7 @@
-WITH 
-noop(id) AS (
-    SELECT task_id FROM timetable.base_task WHERE name = 'NoOp'
-),
-chain_insert(chain_id) AS (
-    INSERT INTO timetable.task_chain 
-        (chain_id, parent_id, task_id, run_uid, database_connection, ignore_error)
-    VALUES 
-        (DEFAULT, NULL, (SELECT id FROM noop), NULL, NULL, TRUE)
-    RETURNING chain_id
-)
 INSERT INTO timetable.chain_execution_config VALUES 
 (
     DEFAULT, -- chain_execution_config, 
-    (SELECT chain_id FROM chain_insert), -- chain_id, 
+    timetable.insert_base_task(task_name := 'NoOp', parent_task_id := NULL), -- chain_id, 
     'execute noop every minute', -- chain_name, 
     NULL, -- run_at_minute, 
     NULL, -- run_at_hour, 
