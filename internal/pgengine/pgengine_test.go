95a5f090f6df909da109ae12ea9def2a14ce75e4
Pavlo Golub <pavlo.golub@gmail.com>
2019-04-30 16:34:56 +0300

    docker test introduced

--- internal/pgengine/pgengine_test.go
+++ internal/pgengine/pgengine_test.go
@@ -1,14 +1,112 @@
 package pgengine_test
 
 import (
+	"database/sql"
 	"fmt"
+	"net"
+	"net/url"
+	"os"
+	"runtime"
 	"testing"
+	"time"
 
 	"github.com/cybertec-postgresql/pg_timetable/internal/pgengine"
 	"github.com/cybertec-postgresql/pg_timetable/internal/tasks"
+	"github.com/ory/dockertest"
+	"github.com/ory/dockertest/docker"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
+
+var pgURL *url.URL
+
+func TestMain(m *testing.M) {
+	pgengine.LogToDB("LOG", "Starting TestMain...")
+	code := 0
+	defer func() {
+		os.Exit(code)
+	}()
+
+	pgURL = &url.URL{
+		Scheme: "postgres",
+		User:   url.UserPassword("scheduler", "scheduler"),
+		Path:   "timetable",
+	}
+	q := pgURL.Query()
+	q.Add("sslmode", "disable")
+	pgURL.RawQuery = q.Encode()
+
+	pool, err := dockertest.NewPool("")
+	if err != nil {
+		pgengine.LogToDB("PANIC", "Could not connect to docker")
+	}
+
+	pw, _ := pgURL.User.Password()
+	runOpts := dockertest.RunOptions{
+		Repository: "postgres",
+		Tag:        "latest",
+		Env: []string{
+			"POSTGRES_USER=" + pgURL.User.Username(),
+			"POSTGRES_PASSWORD=" + pw,
+			"POSTGRES_DB=" + pgURL.Path,
+		},
+	}
+
+	resource, err := pool.RunWithOptions(&runOpts)
+	if err != nil {
+		pgengine.LogToDB("PANIC", "Could start postgres container")
+	}
+	defer func() {
+		err = pool.Purge(resource)
+		if err != nil {
+			pgengine.LogToDB("PANIC", "Could not purge resource")
+		}
+	}()
+
+	pgURL.Host = resource.Container.NetworkSettings.IPAddress
+
+	// Docker layer network is different on Mac
+	if runtime.GOOS == "darwin" {
+		pgURL.Host = net.JoinHostPort(resource.GetBoundIP("5432/tcp"), resource.GetPort("5432/tcp"))
+	}
+
+	logWaiter, err := pool.Client.AttachToContainerNonBlocking(docker.AttachToContainerOptions{
+		Container: resource.Container.ID,
+		// OutputStream: log.Writer(),
+		// ErrorStream:  log.Writer(),
+		Stderr: true,
+		Stdout: true,
+		Stream: true,
+	})
+	if err != nil {
+		pgengine.LogToDB("PANIC", "Could not connect to postgres container log output")
+	}
+
+	defer func() {
+		err = logWaiter.Close()
+		if err != nil {
+			pgengine.LogToDB("ERROR", "Could not close container log")
+		}
+		err = logWaiter.Wait()
+		if err != nil {
+			pgengine.LogToDB("ERROR", "Could not wait for container log to close")
+		}
+	}()
+
+	pool.MaxWait = 10 * time.Second
+	err = pool.Retry(func() error {
+		db, err := sql.Open("postgres", pgURL.String())
+		if err != nil {
+			return err
+		}
+		return db.Ping()
+	})
+	if err != nil {
+		pgengine.LogToDB("PANIC", "Could not connect to postgres server")
+	}
+
+	code = m.Run()
+}
 
 // setupTestDBFunc used to conect and to initialize test PostgreSQL database
 var setupTestDBFunc = func() {
