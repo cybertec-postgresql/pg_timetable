e1fb08ec0dc0be6c932b6055623d447f62923e3f
SanjeevChoubey <schoubey80@gmail.com>
2019-08-31 12:31:42 +0530

    email attachment

--- internal/tasks/mail.go
+++ internal/tasks/mail.go
@@ -1,18 +1,26 @@
 package tasks
 
 import (
+	"crypto/tls"
 	"encoding/json"
-	"net/smtp"
+	"io"
+	"io/ioutil"
+
+	"gopkg.in/gomail.v2"
 )
 
 type emailConn struct {
-	Username   string   `json:"username"`
-	Password   string   `json:"password"`
-	ServerHost string   `json:"serverhost"`
-	ServerPort string   `json:"serverport"`
-	SenderAddr string   `json:"senderaddr"`
-	ToAddr     []string `json:"toaddr"`
-	MsgBody    string   `json:"msgbody"`
+	Username    string   `json:"username"`
+	Password    string   `json:"password"`
+	ServerHost  string   `json:"serverhost"`
+	ServerPort  int      `json:"serverport"`
+	SenderAddr  string   `json:"senderaddr"`
+	CcAddr      []string `json:"ccaddr"`
+	BccAddr     []string `json:"bccaddr"`
+	ToAddr      []string `json:"toaddr"`
+	Subject     string   `json:"subject"`
+	MsgBody     string   `json:"msgbody"`
+	Attachments []string `json:"attachment"`
 }
 
 func taskSendMail(paramValues string) error {
@@ -20,7 +28,49 @@ func taskSendMail(paramValues string) error {
 	if err := json.Unmarshal([]byte(paramValues), &conn); err != nil {
 		return err
 	}
-	addr := conn.ServerHost + ":" + conn.ServerPort
-	auth := smtp.PlainAuth("", conn.Username, conn.Password, conn.ServerHost)
-	return smtp.SendMail(addr, auth, conn.SenderAddr, conn.ToAddr, []byte(conn.MsgBody))
+	mail := gomail.NewMessage()
+	mail.SetHeader("From", conn.SenderAddr)
+
+	//Multiple Recipeints addresses
+	torecipients := make([]string, len(conn.ToAddr))
+	for i, toAddr := range conn.ToAddr {
+		torecipients[i] = mail.FormatAddress(toAddr, " ")
+	}
+	mail.SetHeader("To", torecipients...)
+
+	// Multiple CC Addresses
+	ccrecipients := make([]string, len(conn.CcAddr))
+	for i, ccaddr := range conn.CcAddr {
+		ccrecipients[i] = mail.FormatAddress(ccaddr, " ")
+	}
+	mail.SetHeader("Cc", ccrecipients...)
+
+	// Multiple bCC Addresses
+	bccrecipients := make([]string, len(conn.BccAddr))
+	for i, bccaddr := range conn.BccAddr {
+		bccrecipients[i] = mail.FormatAddress(bccaddr, " ")
+	}
+	mail.SetHeader("Bcc", bccrecipients...)
+
+	mail.SetHeader("Subject", conn.Subject)
+	mail.SetBody("text/html", conn.MsgBody)
+
+	//attach multiple documents
+	for _, attachment := range conn.Attachments {
+		content, err := ioutil.ReadFile(attachment)
+		if err != nil {
+			return err
+		}
+		mail.Attach(attachment, gomail.SetCopyFunc(func(w io.Writer) error {
+			_, err = w.Write(content)
+			return err
+		}))
+	}
+
+	dialer := gomail.NewDialer(conn.ServerHost, conn.ServerPort, conn.Username, conn.Password)
+	dialer.TLSConfig = &tls.Config{
+		InsecureSkipVerify: false,
+		ServerName:         conn.ServerHost,
+	}
+	return dialer.DialAndSend(mail)
 }
--- samples/Mail.sql
+++ samples/Mail.sql
@@ -1,66 +1,45 @@
 DO $$
 	-- An example for using the SendMail task.
 DECLARE
-	v_task_id bigint;
 	v_chain_id bigint;
 	v_chain_config_id bigint;
 BEGIN
-	-- Get the base task id
-	Select task_id INTO v_task_id from timetable.base_task where name='SendMail';
-	
-	-- Create the chain
-	INSERT INTO timetable.task_chain(task_id)
-	VALUES (v_task_id)
-	RETURNING chain_id INTO v_chain_id;
 
-	-- Create the chain execution configuration
-	INSERT INTO timetable.chain_execution_config (chain_id, chain_name, live)
-	VALUES (v_chain_id, 'Send Mail', TRUE)
-	RETURNING chain_execution_config INTO v_chain_config_id;
+	-- Get the chain id
+	v_chain_id := timetable.insert_base_task('SendMail', NULL);
 
-	INSERT INTO timetable.chain_execution_config VALUES 
-    	(
-        DEFAULT, -- chain_execution_config, 
-        v_chain_id, -- chain_id, 
-        'Send Mail', -- chain_name
-        NULL, -- run_at_minute, 
-        NULL, -- run_at_hour, 
-        NULL, -- run_at_day, 
-        NULL, -- run_at_month,
-        NULL, -- run_at_day_of_week, 
-        1, -- max_instances, 
-        TRUE, -- live, 
-        FALSE, -- self_destruct,
-        FALSE, -- exclusive_execution, 
-        NULL -- excluded_execution_configs
-    	)
-    RETURNING  chain_execution_config INTO v_chain_config_id;
+	INSERT INTO timetable.chain_execution_config (chain_id, chain_name, max_instances, live)
+		VALUES (v_chain_id, 'Send Mail', 1, TRUE)
+	RETURNING
+		chain_execution_config INTO v_chain_config_id;
 
 	-- Create the parameters for the chain configuration
-		-- 	"username":   Sender Email Id
-		-- "password":    Sender Mail id Password
-		-- "serverhost":  Your Email Id service provider detail
-		-- "serverport":  Port
-		-- "senderaddr":  Sender Email Id
-		-- "toaddr":      Reciever mail Id, You can add multiple comma separated reciver ids, 
-		-- "msgbody":	  Email Body
-	INSERT INTO timetable.chain_execution_parameters (
-		chain_execution_config,
-		chain_id,
-		order_id,
-		value
-		) 
-		VALUES (
-		v_chain_config_id, v_chain_id, 1, '{
-			"username":   "Userid@example.com", 
-			"password":   "Password", 
-			"serverhost": "smtp.example.com",
-			"serverport":"587",
-			"senderaddr": "Userid@example.com",
-			"toaddr":     ["toAddr@example.com"],
-			"msgbody":	  "Hello, Its pg_timetable test"
-			}'::jsonb
-		);
+		-- "username":	  The username used for authenticating on the mail server
+		-- "password":    The password used for authenticating on the mail server
+		-- "serverhost":  The IP address or hostname of the mail server
+		-- "serverport":  The port of the mail server
+		-- "senderaddr":  The email that will appear as the sender
+		-- "ccaddr":	  String array of the recipients(Cc) email addresses
+		-- "bccaddr":	  String array of the recipients(Bcc) email addresses
+		-- "toaddr":      String array of the recipients(To) email addresses
+		-- "subject":	  Subject of the email
+		-- "attachment":  String array of the attachments
+		-- "msgbody":	  The body of the email
+
+	INSERT INTO timetable.chain_execution_parameters (chain_execution_config, chain_id, order_id, value)
+		VALUES (v_chain_config_id, v_chain_id, 1, '{
+				"username":     "user@example.com",
+				"password":		"password",
+				"serverhost":	"smtp.example.com",
+				"serverport":	587,
+				"senderaddr":   "user@example.com",
+				"ccaddr":		["recipient_cc@example.com"],
+				"bccaddr":		["recipient_bcc@example.com"],
+				"toaddr":       ["recipient@example.com"],
+				"subject": 		"pg_timetable - No Reply",
+				"attachment":   ["D:\\Go stuff\\Books\\Concurrency in Go.pdf","D:\\Go stuff\\Books\\The Way To Go.pdf"],
+				"msgbody":		"<b>Hello User,</b> <p>I got some Go books for you enjoy</p> <i>pg_timetable</i>!"
+				}'::jsonb);
 
 END;
 $$
