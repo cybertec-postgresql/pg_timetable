73f4103723f678e7ccc27b52b851cb0180ceb644
Pavlo Golub <pavlo.golub@gmail.com>
2019-10-04 12:34:00 +0200

    #41 Hide File option. Call os.Exit instead of panic. Always print help in case of error

--- main.go
+++ main.go
@@ -24,7 +24,7 @@ type cmdOptions struct {
 	Port       string `short:"p" long:"port" description:"PG config DB port" default:"5432" env:"PGTT_PGPORT"`
 	Dbname     string `short:"d" long:"dbname" description:"PG config DB dbname" default:"timetable" env:"PGTT_PGDATABASE"`
 	User       string `short:"u" long:"user" description:"PG config DB user" default:"scheduler" env:"PGTT_PGUSER"`
-	File       string `short:"f" long:"file" description:"Config file only mode"`
+	File       string `short:"f" long:"file" description:"Config file only mode" hidden:"TODO"`
 	Password   string `long:"password" description:"PG config DB password" env:"PGCB_PGPASSWORD"`
 	SSLMode    string `long:"sslmode" default:"disable" description:"What SSL priority use for connection" choice:"disable" choice:"require"`
 }
@@ -33,12 +33,15 @@ var cmdOpts cmdOptions
 
 func main() {
 	parser := flags.NewParser(&cmdOpts, flags.PrintErrors|flags.PassDoubleDash)
-	if _, err := parser.Parse(); err != nil {
-		panic(err)
-	}
 	if len(os.Args) < 2 {
 		parser.WriteHelp(os.Stdout)
-		return
+		os.Exit(2)
+	}
+	if _, err := parser.Parse(); err != nil {
+		if !flags.WroteHelp(err) {
+			parser.WriteHelp(os.Stdout)
+			os.Exit(2)
+		}
 	}
 	pgengine.ClientName = cmdOpts.ClientName
 	pgengine.VerboseLogLevel = cmdOpts.Verbose
