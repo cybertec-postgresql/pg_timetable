b20832c67068a0f885a28f6f76bb1d88acfb2e65
Pavlo Golub <pavlo.golub@gmail.com>
2019-10-22 21:43:51 +0200

    Resolves #59

--- samples/Download.sql
+++ samples/Download.sql
@@ -1,62 +1,84 @@
+-- An example for Download task.
 DO $$
-	-- An example for Download task.
 DECLARE
-	v_task_id bigint;
+	v_head_id bigint;
 	v_chain_id bigint;
 	v_chain_config_id bigint;
+	v_task_id bigint;
 BEGIN
-	
-	-- Get the base task id
-	SELECT task_id INTO v_task_id FROM timetable.base_task WHERE name = 'Download';
-	
+	-- Step 1. Download file from the server
 	-- Create the chain
-	INSERT INTO timetable.task_chain(task_id)
-	VALUES (v_task_id)
-	RETURNING chain_id INTO v_chain_id;
+	INSERT INTO timetable.task_chain (task_id)
+	    VALUES (timetable.get_task_id ('Download'))
+	RETURNING
+	    chain_id INTO v_head_id;
 
-	-- Create the chain execution configuration
-	INSERT INTO timetable.chain_execution_config VALUES 
-    	(
-        DEFAULT, -- chain_execution_config, 
-        v_chain_id, -- chain_id, 
-        'Download a file', -- chain_name
-        NULL, -- run_at_minute, 
-        NULL, -- run_at_hour, 
-        NULL, -- run_at_day, 
-        NULL, -- run_at_month,
-        NULL, -- run_at_day_of_week, 
-        1, -- max_instances, 
-        TRUE, -- live, 
-        FALSE, -- self_destruct,
-        FALSE, -- exclusive_execution, 
-        NULL -- excluded_execution_configs
-    )
-    RETURNING  chain_execution_config INTO v_chain_config_id;
+	-- Create the chain execution configuration with default values executed every minute
+	INSERT INTO timetable.chain_execution_config (chain_id, chain_name, live)
+	    VALUES (v_head_id, 'Download locations and aggregate', TRUE)
+	RETURNING
+	    chain_execution_config INTO v_chain_config_id;
 
-	-- Create the parameters for the chain configuration
+	-- Create the parameters for the step 1
+	INSERT INTO timetable.chain_execution_parameters (chain_execution_config, chain_id, order_id, value)
+	    VALUES (v_chain_config_id, v_head_id, 1, '
+	    		{
+	    			"workersnum": 1, 
+	 				"fileurls": ["https://www.cybertec-postgresql.com/secret/orte.txt"], 
+	 				"destpath": "."
+	 			}'::jsonb);
+	
+	RAISE NOTICE 'Step 1 completed. DownloadFile task added';
 
-		--"workersnum":   Workers nummber - If the requested number of workers is less than one, a worker will be created
-		--                for every request. 
-		-- "fileurls":    Provide urls from where you wanna download files, User can mention n number of 
-		--                comma separated urls 
-		--"destpath":     Destination path 
+	-- Step 2. Transform Unicode characters into ASCII
+	-- Create the shell task to call 'uconv -x' and name it 'unaccent'
+	INSERT INTO timetable.base_task(name, kind, script)
+		VALUES ('unaccent', 'SHELL'::timetable.task_kind, 'uconv -x "::Latin; ::Latin-ASCII;"')
+	RETURNING 
+		task_id INTO v_task_id;
 
-	INSERT INTO timetable.chain_execution_parameters (
-		chain_execution_config,
-		chain_id,
-		order_id,
-		value
-	) VALUES (
-		v_chain_config_id,
-		v_chain_id, 
-		1, 
-		'{
-			"workersnum":   1, 
-			"fileurls":   ["http://www.golang-book.com/public/pdf/gobook.pdf"], 
-			"destpath": ".."
-		}'::jsonb
-		);
+	-- Add shell task 'unaccent' to the chain
+	INSERT INTO timetable.task_chain (parent_id, task_id, ignore_error)
+		VALUES (v_head_id, v_task_id, TRUE)
+	RETURNING
+	    chain_id INTO v_chain_id;
 
+	-- Create the parameters for the 'unaccent' base task. Input and output files in this case
+	INSERT INTO timetable.chain_execution_parameters (chain_execution_config, chain_id, order_id, value)
+	    VALUES (v_chain_config_id, v_chain_id, 2, '[
+	 			"<", "orte.txt",
+	 			">", "orte_ansi.txt"
+	 		]'::jsonb);
+
+	RAISE NOTICE 'Step 2 completed. Unacent task added';
+
+	-- Step 3. Import ASCII file to PostgreSQL table using "psql \copy"
+	-- Create the shell task to cal 'psql' and name it 'psql'
+	INSERT INTO timetable.base_task(name, kind, script)
+		VALUES ('psql', 'SHELL'::timetable.task_kind, 'psql')
+	RETURNING 
+		task_id INTO v_task_id;
+
+	-- Add shell task 'psql' to the chain
+	INSERT INTO timetable.task_chain (parent_id, task_id)
+		VALUES (v_chain_id, v_task_id)
+	RETURNING
+	    chain_id INTO v_chain_id;
+
+	-- Prepare the destination table 'location'
+	CREATE TABLE IF NOT EXISTS location(name text);
+
+	-- Add the parameters for the 'psql' base task. Execute client side \copy to 'location' from 'orte_ansi.txt'
+	INSERT INTO timetable.chain_execution_parameters (chain_execution_config, chain_id, order_id, value)
+	    VALUES (v_chain_config_id, v_chain_id, 3, ('[
+			"-h", "' || host(inet_server_addr()) || '",
+			"-p", "' || inet_server_port() || '",
+			"-d", "' || current_database() || '",
+			"-U", "' || current_user || '",
+			"-c", "\\copy location FROM orte_ansi.txt"
+		]')::jsonb);
+
+	RAISE NOTICE 'Step 3 completed. Import task added';
 END;
 $$
 LANGUAGE 'plpgsql';
