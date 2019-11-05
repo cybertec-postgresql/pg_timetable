7a9b6ac44f8192d68c446f27132ebf2f339b442e
Pavlo Golub <pavlo.golub@gmail.com>
2019-11-05 17:36:51 +0200

    Fixes  #67: "Password ignored for connection"

--- internal/pgengine/bootstrap.go
+++ internal/pgengine/bootstrap.go
@@ -56,8 +56,8 @@ func PrefixSchemaFiles(prefix string) {
 func InitAndTestConfigDBConnection(schemafiles []string) {
 	var wt int = waitTime
 	var err error
-	connstr := fmt.Sprintf("application_name=pg_timetable host='%s' port='%s' dbname='%s' sslmode='%s' user='%s'",
-		Host, Port, DbName, SSLMode, User)
+	connstr := fmt.Sprintf("application_name=pg_timetable host='%s' port='%s' dbname='%s' sslmode='%s' user='%s' password='%s'",
+		Host, Port, DbName, SSLMode, User, Password)
 	LogToDB("DEBUG", "Connection string: ", connstr)
 	ConfigDb, err = sqlx.Connect("postgres", connstr)
 	for err != nil {
