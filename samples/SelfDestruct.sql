6786d0fd4e5164602b8b447db0ae3c8c3bffa421
Pavlo Golub <pavlo.golub@gmail.com>
2020-02-04 19:33:04 +0100

    - self_destruct sample added

---
+++ samples/SelfDestruct.sql
@@ -0,0 +1,39 @@
+WITH 
+sql_task(id) AS (
+    INSERT INTO timetable.base_task VALUES (
+		DEFAULT, 					-- task_id
+		'self destruct task',	    -- name
+		DEFAULT, 					-- 'SQL' :: timetable.task_kind
+		'SELECT raise_func($1)'		-- task script
+	)
+	RETURNING task_id
+),
+chain_insert(chain_id) AS (
+    INSERT INTO timetable.task_chain 
+        (chain_id, parent_id, task_id, run_uid, database_connection, ignore_error)
+    VALUES 
+        (DEFAULT, NULL, (SELECT id FROM sql_task), NULL, NULL, TRUE)
+    RETURNING chain_id
+),
+chain_config(id) as (
+    INSERT INTO timetable.chain_execution_config VALUES 
+    (
+        DEFAULT, -- chain_execution_config, 
+        (SELECT chain_id FROM chain_insert), -- chain_id, 
+        'notify then destruct', -- chain_name, 
+        '* * * * *', -- run_at, 
+        1, -- max_instances, 
+        TRUE, -- live, 
+        TRUE, -- self_destruct,
+        FALSE, -- exclusive_execution, 
+        NULL -- excluded_execution_configs
+    )
+    RETURNING  chain_execution_config
+)
+INSERT INTO timetable.chain_execution_parameters 
+    (chain_execution_config, chain_id, order_id, value)
+VALUES (
+    (SELECT id FROM chain_config),
+    (SELECT chain_id FROM chain_insert),
+    1,
+    '[ "TT_CHANNEL", "Ahoi from SQL base task" ]' :: jsonb) 
