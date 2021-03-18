fa2bc4a294d027e890dbd4dc950968ff25cc343c
Pavlo Golub <pavlo.golub@gmail.com>
2021-03-18 16:40:26 +0100

    [+] allow specify content-type for SendMail built-in task, closes #225

--- internal/tasks/mail.go
+++ internal/tasks/mail.go
@@ -16,6 +16,7 @@ type EmailConn struct {
 	Subject     string   `json:"subject"`
 	MsgBody     string   `json:"msgbody"`
 	Attachments []string `json:"attachment"`
+	ContentType string   `json:"contenttype"`
 }
 
 // Dialer implements DialAndSend function for mailer
@@ -53,7 +54,7 @@ func SendMail(conn EmailConn) error {
 	mail.SetHeader("Bcc", bccrecipients...)
 
 	mail.SetHeader("Subject", conn.Subject)
-	mail.SetBody("text/html", conn.MsgBody)
+	mail.SetBody(conn.ContentType, conn.MsgBody)
 
 	//attach multiple documents
 	for _, attachment := range conn.Attachments {
