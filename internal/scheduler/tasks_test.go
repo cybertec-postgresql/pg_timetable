e17c799d5f91eaddb9bc5ecd20dbf88ee085e63a
Pavlo Golub <pavlo.golub@gmail.com>
2021-03-18 10:02:40 +0100

    add tests for taskCopyFromFile

--- internal/scheduler/tasks_test.go
+++ internal/scheduler/tasks_test.go
@@ -17,9 +17,16 @@ func TestExecuteTask(t *testing.T) {
 	mocksch := New(pge)
 
 	assert.Error(t, mocksch.executeTask(context.TODO(), "foo", []string{}))
+
 	assert.Error(t, mocksch.executeTask(context.TODO(), "Sleep", []string{"foo"}))
 	assert.NoError(t, mocksch.executeTask(context.TODO(), "Sleep", []string{"1"}))
+
 	assert.NoError(t, mocksch.executeTask(context.TODO(), "NoOp", []string{}))
 	assert.NoError(t, mocksch.executeTask(context.TODO(), "NoOp", []string{"foo", "bar"}))
+
 	assert.NoError(t, mocksch.executeTask(context.TODO(), "Log", []string{"foo"}))
+
+	assert.Error(t, mocksch.executeTask(context.TODO(), "CopyFromFile", []string{"foo"}), "Invalid json")
+	assert.Error(t, mocksch.executeTask(context.TODO(), "CopyFromFile",
+		[]string{`{"sql": "COPY foo from STDIN", "filename": "foo"}`}), "Acquire() should fail")
 }
