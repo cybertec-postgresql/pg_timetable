18a3f8a5eb762baa27e080de082a453fa36e7d49
Pavlo Golub <pavlo.golub@gmail.com>
2021-04-27 09:44:48 +0200

    [+] add download.sql to samples.rst

--- docs/samples.rst
+++ docs/samples.rst
@@ -9,3 +9,12 @@ This sample demonstrates how to create a basic one-step chain with parameters. I
 .. literalinclude:: ../samples/basic.sql
     :linenos:
     :language: SQL
+
+Download, Transform and Import
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+This sample demonstrates how to create enhanced three-step chain with parameters. It uses DO statement to directly update the
+**timetable** schema tables.
+
+.. literalinclude:: ../samples/Download.sql
+    :linenos:
+    :language: SQL
--- samples/Download.sql
+++ samples/Download.sql
@@ -1,9 +1,12 @@
--- An example for Download task.
+-- An enhanced example consisting of three tasks:
+-- 1. Download text file from internet using BUILT-IN command
+-- 2. Remove accents (diacritic signs) from letters using PROGRAM command (can be done with `unaccent` PostgreSQL extension) 
+-- 3. Import text file as CSV file using BUILT-IN command (can be down with `psql -c /copy`)
 DO $$
 DECLARE
 	v_head_id bigint;
 	v_task_id bigint;
-	v_chain_config_id bigint;
+	v_chain_id bigint;
 	v_command_id bigint;
 BEGIN
 	-- Step 1. Download file from the server
@@ -13,27 +16,27 @@ BEGIN
 	RETURNING
 	    task_id INTO v_head_id;
 
-	-- Create the chain execution configuration with default values executed every minute
+	-- Create the chain with default values executed every minute (NULL == '* * * * *' :: timetable.cron)
 	INSERT INTO timetable.chain 
 		(task_id, chain_name, live)
 	VALUES 
 		(v_head_id, 'Download locations and aggregate', TRUE)
 	RETURNING
-	    chain_id INTO v_chain_config_id;
+	    chain_id INTO v_chain_id;
 
-	-- Create the parameters for the step 1
+	-- Create the parameters for the step 1:
 	INSERT INTO timetable.parameter (chain_id, task_id, order_id, value)
-		VALUES (v_chain_config_id, v_head_id, 1, '
+		VALUES (v_chain_id, v_head_id, 1, '
 				{
-					"workersnum": 1, 
+					"workersnum": 1,
 					"fileurls": ["https://www.cybertec-postgresql.com/secret/orte.txt"], 
 					"destpath": "."
 				}'::jsonb);
 	
-	RAISE NOTICE 'Step 1 completed. DownloadFile task added with ID: %', v_chain_config_id;
+	RAISE NOTICE 'Step 1 completed. DownloadFile task added with ID: %', v_chain_id;
 
 	-- Step 2. Transform Unicode characters into ASCII
-	-- Create the program task to call 'uconv -x' and name it 'unaccent'
+	-- Create the program task to call 'uconv' and name it 'unaccent'
 	INSERT INTO timetable.command(name, kind, script)
 		VALUES ('unaccent', 'PROGRAM'::timetable.command_kind, 'uconv')
 	RETURNING 
@@ -45,11 +48,11 @@ BEGIN
 	RETURNING
 	    task_id INTO v_task_id;
 
-	-- Create the parameters for the 'unaccent' base task. Input and output files in this case
+	-- Create the parameters for the 'unaccent' task. Input and output files in this case
 	-- Under Windows we should call PowerShell instead of "uconv" with command:
 	-- Set-content "orte_ansi.txt" ((Get-content "orte.txt").Normalize("FormD") -replace '\p{M}', '')
 	INSERT INTO timetable.parameter (chain_id, task_id, order_id, value)
-	    VALUES (v_chain_config_id, v_task_id, 1, '["-x", "Latin-ASCII", "-o", "orte_ansi.txt", "orte.txt"]'::jsonb);
+	    VALUES (v_chain_id, v_task_id, 1, '["-x", "Latin-ASCII", "-o", "orte_ansi.txt", "orte.txt"]'::jsonb);
 
 	RAISE NOTICE 'Step 2 completed. Unacent task added';
 
@@ -62,11 +65,10 @@ BEGIN
 	-- Prepare the destination table 'location'
 	CREATE TABLE IF NOT EXISTS location(name text);
 
-	-- Add the parameters for the 'psql' base task. Execute client side \copy to 'location' from 'orte_ansi.txt'
+	-- Add the parameters for the download task. Execute client side COPY to 'location' from 'orte_ansi.txt'
 	INSERT INTO timetable.parameter (chain_id, task_id, order_id, value)
-	    VALUES (v_chain_config_id, v_task_id, 1, '{"sql": "COPY location FROM STDIN", "filename": "orte_ansi.txt" }'::jsonb);
+	    VALUES (v_chain_id, v_task_id, 1, '{"sql": "COPY location FROM STDIN", "filename": "orte_ansi.txt" }'::jsonb);
 
 	RAISE NOTICE 'Step 3 completed. Import task added';
 END;
-$$
-LANGUAGE 'plpgsql';
+$$ LANGUAGE PLPGSQL;
