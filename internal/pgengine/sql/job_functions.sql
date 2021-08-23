a1138949dc1d08fa041acf97b70e5485376bc354
Pavlo Golub <pavlo.golub@gmail.com>
2021-08-23 18:04:36 +0200

    [+] add `move_task_up()` and `move_task_down()` functions, closes #314

--- internal/pgengine/sql/job_functions.sql
+++ internal/pgengine/sql/job_functions.sql
@@ -73,9 +73,9 @@ CREATE OR REPLACE FUNCTION timetable.notify_chain_start(
     chain_id BIGINT, 
     worker_name TEXT
 ) RETURNS void AS $$
-  SELECT pg_notify(
-      worker_name, 
-    format('{"ConfigID": %s, "Command": "START", "Ts": %s}', 
+    SELECT pg_notify(
+        worker_name, 
+        format('{"ConfigID": %s, "Command": "START", "Ts": %s}', 
         chain_id, 
         EXTRACT(epoch FROM clock_timestamp())::bigint)
     )
@@ -85,11 +85,54 @@ CREATE OR REPLACE FUNCTION timetable.notify_chain_stop(
     chain_id BIGINT, 
     worker_name TEXT
 ) RETURNS void AS  $$ 
-  SELECT pg_notify(
-      worker_name, 
-    format('{"ConfigID": %s, "Command": "STOP", "Ts": %s}', 
-        chain_id, 
-        EXTRACT(epoch FROM clock_timestamp())::bigint)
-    )
+    SELECT pg_notify(
+        worker_name, 
+        format('{"ConfigID": %s, "Command": "STOP", "Ts": %s}', 
+            chain_id, 
+            EXTRACT(epoch FROM clock_timestamp())::bigint)
+        )
 $$ LANGUAGE SQL;
 
+CREATE OR REPLACE FUNCTION timetable.move_task_up(IN task_id BIGINT) RETURNS boolean AS $$
+    WITH 
+    current_task AS (
+        SELECT * FROM timetable.task WHERE task_id = $1), 
+    parrent_task AS (
+        SELECT t.* FROM timetable.task t, current_task WHERE t.task_id = current_task.parent_id),
+    upd_parent AS (
+        UPDATE timetable.task t SET 
+            (task_name, kind, command, run_as, database_connection, ignore_error, autonomous, timeout) = 
+            (ct.task_name, ct.kind, ct.command, ct.run_as, ct.database_connection, ct.ignore_error, ct.autonomous, ct.timeout) 
+        FROM current_task ct WHERE t.task_id = ct.parent_id
+    ),
+    upd_current AS (
+        UPDATE timetable.task t SET 
+            (task_name, kind, command, run_as, database_connection, ignore_error, autonomous, timeout) = 
+            (pt.task_name, pt.kind, pt.command, pt.run_as, pt.database_connection, pt.ignore_error, pt.autonomous, pt.timeout) 
+        FROM parrent_task pt WHERE t.task_id = $1
+        RETURNING true
+    )
+    SELECT count(*) > 0 FROM upd_current
+$$ LANGUAGE SQL;
+
+CREATE OR REPLACE FUNCTION timetable.move_task_down(IN task_id BIGINT) RETURNS boolean AS $$
+    WITH 
+    current_task AS (
+        SELECT * FROM timetable.task WHERE task_id = $1), 
+    child_task AS (
+        SELECT * FROM timetable.task WHERE parent_id = $1),
+    upd_child AS (
+        UPDATE timetable.task t SET 
+            (task_name, kind, command, run_as, database_connection, ignore_error, autonomous, timeout) = 
+            (ct.task_name, ct.kind, ct.command, ct.run_as, ct.database_connection, ct.ignore_error, ct.autonomous, ct.timeout) 
+        FROM current_task ct WHERE t.parent_id = $1
+    ),
+    upd_current AS (
+        UPDATE timetable.task t SET 
+            (task_name, kind, command, run_as, database_connection, ignore_error, autonomous, timeout) = 
+            (pt.task_name, pt.kind, pt.command, pt.run_as, pt.database_connection, pt.ignore_error, pt.autonomous, pt.timeout) 
+        FROM child_task pt WHERE t.task_id = $1
+        RETURNING true
+    )
+    SELECT count(*) > 0 FROM upd_current
+$$ LANGUAGE SQL;
