0288f1053bbc476b8ddce600df7a96f0cde921ae
ukolovda <udmitry@mail.ru>
2021-10-10 11:42:16 +0400

    Fix using env variable $PGTT_CLIENTNAME as Client Name.

--- internal/config/config_test.go
+++ internal/config/config_test.go
@@ -23,4 +23,9 @@ func TestConfig(t *testing.T) {
 	os.Args = []string{0: "config_test", "--config=foo.boo.bar.baz.yaml"}
 	_, err = NewConfig(nil)
 	assert.Error(t, err)
+
+	os.Args = []string{0: "config_test"} // clientname arg is missing, but set PGTT_CLIENTNAME
+	assert.NoError(t, os.Setenv("PGTT_CLIENTNAME", "worker001"))
+	_, err = NewConfig(nil)
+	assert.NoError(t, err)
 }
