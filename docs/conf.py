3f08be837688b6e3309e4524eba4cd390dd5a2bc
Pavlo Golub <pavlo.golub@gmail.com>
2023-07-11 13:42:56 +0200

    [+] add a Sphinx `conf.py` file to documentation, fixes #590

---
+++ docs/conf.py
@@ -0,0 +1,28 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+extensions = []
+templates_path = ['templates', '_templates', '.templates']
+source_suffix = ['.rst', '.md']
+source_parsers = {
+            '.md': CommonMarkParser,
+        }
+project = u'pg_timetable'
+copyright = str(datetime.now().year)
+
+# -- Options for EPUB output
+epub_show_urls = "footnote"
+
+exclude_patterns = ['_build']
+pygments_style = 'sphinx'
+htmlhelp_basename = 'pg-timetable'
+html_theme = 'sphinx_rtd_theme'
+file_insertion_enabled = False
+latex_documents = [
+  ('index', 'pg-timetable.tex', u'pg_timetable Documentation',
+   u'', 'manual'),
+]
+
